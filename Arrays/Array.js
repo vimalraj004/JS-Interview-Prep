f79e6b71c72dd920b6d0a5d3b1e0ecb8d5f1939c
vimalraj <vimalrajvj1048@gmail.com>
2026-08-08 11:44:36 +0530

    first commit

---
+++ Arrays/Array.js
@@ -0,0 +1,9 @@
+// let arr = [10, 8, [2, 3], 4, 2, [1, 1, 1], 5, 7, [4, 2], 0];
+
+// Move the nested arrays based on their sum in ascending order, while keeping all non-array elements in their original positions.
+
+// Expected output:
+
+// [10, 8, [1, 1, 1], 4, 2, [2, 3], 5, 7, [2, 4], 0]
+
+// Interviewer: Explain your approach and write the JavaScript code.
